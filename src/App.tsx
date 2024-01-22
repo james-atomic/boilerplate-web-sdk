d6e14e0254c1decec1f5f384555d72adf01073f1
Ithrendrial <rcox2990@gmail.com>
2024-01-23 11:46:03 +1300

    add vertical stream and launcher containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { configAtomicSDK } from "./config/configAtomicSDK";
 import { SingleCard } from "./components/singleCard";
+import { Launcher } from "./components/launcher";
+import { VerticalStream } from "./components/verticalStream";
 import "./App.css";
+import { SDKConfiguration } from "@atomic.io/action-cards-web-sdk";
 
 const CONTAINER_ID = configAtomicSDK();
 
 const App = () => {
+    // Configure the containers
+    const config: SDKConfiguration = {
+        streamContainerId: CONTAINER_ID,
+        onRuntimeVariablesRequested: (cards, callback) : void => {
+            cards.forEach(function(card) : void {
+                // Replace the name of the runtime variable with your own set in the Workbench and modify what you want the value to be.
+                card.runtimeVariables.set('test_variable', 'If you see this, then it works! Yay!');
+            })
+
+            callback(cards);
+        },
+        // Customisation of the containers' UI. Other options can be found in the WebSDK documentation
+        enabledUiElements: {
+            launcherButton: {
+                disabled: false,
+                backgroundColor: '#43b611'
+            }
+        },
+        customStrings: {
+            cardListTitle: "Custom List Title"
+        }
+    };
+
     return (
         <>
-            <SingleCard containerId={ CONTAINER_ID } />
+            <Launcher config={ config }/>
+            <VerticalStream config={ config }/>
+            <SingleCard config={ config }/>
         </>
     );
 };
--- src/components/verticalStream.tsx
+++ src/components/verticalStream.tsx
@@ -1,30 +1,29 @@
-import React, {useCallback, useRef} from "react";
+import React, {useCallback, useRef} from 'react';
 import AtomicSDK, {AACStreamContainer, SDKConfiguration} from "@atomic.io/action-cards-web-sdk";
-import styles from "../styles/SingleCard.module.css"
-
-interface SingleCardProps {
+import styles from "../styles/VerticalStream.module.css"
+interface VerticalStreamProps {
     config: SDKConfiguration
 }
 
-const SingleCard: React.FC<SingleCardProps> = ({ config }) => {
+const VerticalStream: React.FC<VerticalStreamProps> = ({ config }) => {
     // Create a reference to the stream container and clean up any existing instances before
     // creating a new one to render the single card view.
     const embedRef = useRef<AACStreamContainer>();
+    // Remove any potentially existing instances
     const initAtomicEmbed = useCallback((element: HTMLDivElement) => {
-        // Remove any potentially existing instances
         if (embedRef.current) {
             embedRef.current.stop();
         }
 
         if (element) {
             // If 'element' exists, initialise the embedded container
-            embedRef.current = AtomicSDK.singleCard(element, config);
+            embedRef.current = AtomicSDK.embed(element, config);
         }
     }, []);
 
     return (
         <div ref={ initAtomicEmbed } className={ styles.card_container_wrapper }/>
     );
-};
+}
 
-export { SingleCard };
+export { VerticalStream };
