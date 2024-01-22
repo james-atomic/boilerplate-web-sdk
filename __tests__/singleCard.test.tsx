226713e57d9755256ee5f6bd4a7d9a8a63ed50bc
Ithrendrial <rcox2990@gmail.com>
2024-01-23 11:58:24 +1300

    fix unit tests after refactor

--- __tests__/singleCard.test.tsx
+++ __tests__/singleCard.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { SingleCard } from '../src/components/singleCard';
+import {VerticalStream} from "../src/components/verticalStream";
+import {Launcher} from "../src/components/launcher";
 
+const mock_config =  { streamContainerId: 'abc'};
 it('render a single card container without crashing', () => {
-    const id = "abc"
-    TestRenderer.create(<SingleCard containerId={ id }/>);
+    TestRenderer.create(<SingleCard config={ mock_config }/>);
 });
+
+it('render a vertical stream container without crashing', () => {
+    TestRenderer.create(<VerticalStream config={ mock_config }/>);
+});
+
+it('render a launcher without crashing', () => {
+    TestRenderer.create(<Launcher config={ mock_config }/>);
+});
+
