c7a537efd77a89c01521fd91076b30fffd2d3568
Ithrendrial <rcox2990@gmail.com>
2024-01-30 10:13:57 +1300

    add styling and a view selector system

--- __tests__/singleCard.test.tsx
+++ __tests__/singleCard.test.tsx
@@ -1,19 +1,2 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import { SingleCard } from '../src/components/singleCard';
-import {VerticalStream} from "../src/components/verticalStream";
-import {Launcher} from "../src/components/launcher";
-
-const mock_config =  { streamContainerId: 'abc'};
-it('render a single card container without crashing', () => {
-    TestRenderer.create(<SingleCard config={ mock_config }/>);
-});
-
-it('render a vertical stream container without crashing', () => {
-    TestRenderer.create(<VerticalStream config={ mock_config }/>);
-});
-
-it('render a launcher without crashing', () => {
-    TestRenderer.create(<Launcher config={ mock_config }/>);
-});
-
--- src/components/launcher/index.tsx
+++ src/components/launcher/index.tsx
@@ -1,16 +1,27 @@
 import React, { useEffect } from "react";
 import AtomicSDK, { SDKConfiguration } from "@atomic.io/action-cards-web-sdk";
-
+import styles from './Launcher.module.css'
 interface LauncherProps {
   config: SDKConfiguration;
+  visible: boolean;
 }
 
-export const Launcher = ({ config }: LauncherProps) => {
+export const Launcher = (props: LauncherProps) => {
   useEffect(() => {
-    const instance = AtomicSDK.launch(config);
+    if (props.visible) {
+      const instance = AtomicSDK.launch(props.config);
+      return () => instance.stop();
+    }
+  }, [props.visible]);
 
-    return () => instance.stop();
-  }, []);
-
-  return <></>;
+  return <>
+    <div className={props.visible ? styles.launcher_wrapper : styles.none}>
+      <div className={styles.info_wrapper}>
+        <div className={styles.title}>Launcher</div>
+        <p>The launcher is a method of displaying your cards that implements a trigger button to open and close the stream container. Once opened, the stream container automatically resizes itself to fit its content.
+        Usually, the trigger button is located in the bottom right of the screen, however, as shown in this example, you can reposition the trigger button and container using CSS selectors.
+        <br /><br />You can read more about it here in the <a href="https://documentation.atomic.io/sdks/web#displaying-a-launcher" target="_blank">launcher documentation.</a></p>
+      </div>
+    </div>
+  </>;
 };
